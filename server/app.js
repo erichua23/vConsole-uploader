115165806fbc206a82a032e235c72a16cea56310
erichua(华挺) <erichua@webank.com>
2016-12-23 12:09:29 +0800

    add logview

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ var app = express();
 var config = require('./config');
 app.configure(function() {
   app.set('config', config); 
-}
+});
 
 // TODO add ip filter here
 app.use(ipfilter([], {mode: 'allow'})); 
